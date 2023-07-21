0c17dbf24dbca7c30ce1cdfa51066aebf529a2fc
TheMarvello <dipukumars7476@gmail.com>
2023-07-22 00:58:35 +0530

    First commit

---
+++ src/Components/Temp.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from "react";
+import Weathercard from "./WeatherCard.js";
+import "./styles.css";
+
+const Temp = () => {
+  const [searchValue, setSearchValue] = useState("Jamshedpur");
+  const [tempInfo, setTempInfo] = useState({});
+
+  const getWeatherInfo = async () => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=e47b84897cd9d94fc622c014d6517dc4`;
+
+      let res = await fetch(url);
+      let data = await res.json();
+
+      const { temp, humidity, pressure } = data.main;
+      const { main: weathermood } = data.weather[0];
+      const { name } = data;
+      const { speed } = data.wind;
+      const { country, sunset } = data.sys;
+
+      const myNewWeatherInfo = {
+        temp,
+        humidity,
+        pressure,
+        weathermood,
+        name,
+        speed,
+        country,
+        sunset,
+      };
+
+      setTempInfo(myNewWeatherInfo);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getWeatherInfo();
+  }, []);
+
+  const handleKeyDown = (event) => {
+    console.log(event);
+    if(event.key === 'Enter')
+    return getWeatherInfo();
+  }
+
+  return (
+    <>
+      <div className="wrap">
+        <div className="search">
+          <input
+            type="search"
+            placeholder="search..."
+            autoFocus
+            id="search"
+            className="searchTerm"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
+
+          <button
+            className="searchButton"
+            type="submit"
+            onClick={getWeatherInfo}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+
+      {/* our temp card  */}
+      <Weathercard {...tempInfo} />
+    </>
+  );
+};
+
+export default Temp;
